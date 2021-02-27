e0ca9ee90083f7211046e9003c6862a2f57827b8
Rohan Gautam <rohan1gautam@gmail.com>
2021-02-27 17:29:14 +0800

    set a small/medium tree anywhere

--- garden/index.js
+++ garden/index.js
@@ -1,8 +1,43 @@
-let tileIdMap;
+const treePositionMap = {
+  1: "-1 0 -1",
+  2: "0 0 -1",
+  3: "1 0 -1",
+  4: "-1 0 0",
+  5: "0 0 0",
+  6: "1 0 0",
+  7: "-1 0 1",
+  8: "0 0 1",
+  9: "1 0 1",
+};
+const plantModelMap = {
+  1: "plant-model-1",
+  2: "plant-model-2",
+  3: "plant-model-3",
+  4: "plant-model-4",
+  5: "plant-model-5",
+  6: "plant-model-6",
+  7: "plant-model-7",
+  8: "plant-model-8",
+  9: "plant-model-9",
+};
+// const tileIdMap = {
+//   1: "tile-1",
+//   2: "tile-2",
+//   3: "tile-3",
+//   4: "tile-4",
+//   5: "tile-5",
+//   6: "tile-6",
+//   7: "tile-7",
+//   8: "tile-8",
+//   9: "tile-9",
+// };
 
-// let small_tree = document.getElementById("medium-tree");
-// small_tree.setAttribute("scale", "1 1 1");
-// small_tree.setAttribute(
-//   "obj-model",
-//   "obj: #small-tree-obj; mtl: #small-tree-mtl"
-// );
+const SMALL_TREE_MODEL = "obj: #small-tree-obj; mtl: #small-tree-mtl";
+const MED_TREE_MODEL = "obj: #medium-tree-obj; mtl: #medium-tree-mtl";
+
+let tile_num = 8;
+let growth_num = 2;
+let tree = document.getElementById(plantModelMap[tile_num]);
+tree.setAttribute("scale", "0.5 0.5 0.5");
+tree.setAttribute("position", treePositionMap[tile_num]);
+tree.setAttribute("obj-model", MED_TREE_MODEL);

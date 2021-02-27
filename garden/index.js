1df02b74a141423e21f95cb432e76a72e5aa842a
Rohan Gautam <rohan1gautam@gmail.com>
2021-02-27 16:40:37 +0800

    have a medium sized tree, dynamically change model

--- garden/index.js
+++ garden/index.js
@@ -1,2 +1,6 @@
-let small_tree = document.getElementById("small-tree");
-small_tree.setAttribute("scale", "1 3 1");
+let small_tree = document.getElementById("medium-tree");
+small_tree.setAttribute("scale", "1 1 1");
+small_tree.setAttribute(
+  "obj-model",
+  "obj: #small-tree-obj; mtl: #small-tree-mtl"
+);

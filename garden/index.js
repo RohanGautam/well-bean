e0579a3a80d03d6733c258e26e2f8b201e8eb739
Rohan Gautam <rohan1gautam@gmail.com>
2021-02-27 17:03:19 +0800

    add id's that make it less chaotic

--- garden/index.js
+++ garden/index.js
@@ -1,6 +1,8 @@
-let small_tree = document.getElementById("medium-tree");
-small_tree.setAttribute("scale", "1 1 1");
-small_tree.setAttribute(
-  "obj-model",
-  "obj: #small-tree-obj; mtl: #small-tree-mtl"
-);
+let tileIdMap;
+
+// let small_tree = document.getElementById("medium-tree");
+// small_tree.setAttribute("scale", "1 1 1");
+// small_tree.setAttribute(
+//   "obj-model",
+//   "obj: #small-tree-obj; mtl: #small-tree-mtl"
+// );
